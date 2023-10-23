11014e549ef7779a455a1c970a7ee628dd0cfe8b
quantumwall <liberiamor@gmail.com>
2023-10-23 12:12:12 +0300

    init commit

---
+++ src/main/java/dev/zykov/expatcalc/ExpatCalculatorApplication.java
@@ -0,0 +1,13 @@
+package dev.zykov.expatcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpatCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpatCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zykov/expatcalc/ExpatCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zykov.expatcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpatCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
