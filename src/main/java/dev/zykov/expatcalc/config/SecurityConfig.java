ec7892d9a26cc07377b21933ea5e47f1f763dbe4
quantumwall <liberiamor@gmail.com>
2023-11-03 09:53:03 +0300

    add initial oauth2 security

---
+++ src/main/java/dev/zykov/expatcalc/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package dev.zykov.expatcalc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).oauth2Login(Customizer.withDefaults())
+            .build();
+    }
+}
