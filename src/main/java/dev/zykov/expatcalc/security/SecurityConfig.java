0a3d4bbb87bc05111d1ef0ff53a372d5a3a34ce0
quantumwall <liberiamor@gmail.com>
2023-11-09 13:58:52 +0300

    add authorize endpoint to redirect user after auth

--- src/main/java/dev/zykov/expatcalc/security/SecurityConfig.java
+++ src/main/java/dev/zykov/expatcalc/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package dev.zykov.expatcalc.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import dev.zykov.expatcalc.security.oidc.CustomOidcUserService;
@@ -11,6 +11,7 @@ import dev.zykov.expatcalc.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -19,12 +20,11 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filter(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-        http.cors(cors -> cors.disable());
-        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.authorizeHttpRequests(
-                auth -> auth.requestMatchers("/", "/login").permitAll().anyRequest().authenticated());
-        http.oauth2Login(login -> login.userInfoEndpoint(ui -> ui.oidcUserService(customOidcUserService)));
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.oauth2Login(login -> {
+            login.defaultSuccessUrl("/authorize", true);
+        });
+//        http.oauth2Login(login -> login.userInfoEndpoint(ui -> ui.oidcUserService(customOidcUserService)));
         return http.build();
     }
 }
